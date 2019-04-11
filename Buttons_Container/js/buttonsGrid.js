1bc46dc221a7e95d4108fe48b5575df3017b96bf
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-11 21:56:11 +0530

    Added the folder for Buttons Container

---
+++ Buttons_Container/js/buttonsGrid.js
@@ -0,0 +1,39 @@
+function btnClick(){
+
+      var one = document.getElementById("btn1").innerText;
+       var two = document.getElementById("btn2").innerText;
+    
+
+      var three = document.getElementById("btn3").innerText;
+
+      
+
+      var four = document.getElementById("btn4").innerText;
+
+      var six = document.getElementById("btn6").innerText;
+
+      var seven = document.getElementById("btn7").innerText;
+
+      var eight = document.getElementById("btn8").innerText;
+
+      var nine = document.getElementById("btn9").innerText;
+
+      
+
+      document.getElementById("btn1").innerText = four;
+
+      document.getElementById("btn2").innerText = one;
+
+      document.getElementById("btn3").innerText = two;
+
+      document.getElementById("btn4").innerText = seven;
+
+      document.getElementById("btn6").innerText = three;
+
+      document.getElementById("btn7").innerText = eight;
+
+      document.getElementById("btn8").innerText = nine;
+
+      document.getElementById("btn9").innerText = six;
+
+  }
