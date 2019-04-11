ef968839542080ca566fe8964ba28955f2fd9771
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-11 22:09:55 +0530

    a program on implementing loops

---
+++ Loops.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    let consonant;
+    let vowel;
+
+    var x = 0;
+    for (x = 0; x <= s.length; x++) {
+        if (s[x] == 'a' || s[x] == 'e' || s[x] == 'i' || s[x] == 'o' || s[x] == 'u') {
+            console.log(s[x]);
+        }
+
+    }
+    for (x = 0; x < s.length; x++) {
+        if (s[x] == 'a' || s[x] == 'e' || s[x] == 'i' || s[x] == 'o' || s[x] == 'u') {
+            continue;
+        }
+        else {
+            console.log(s[x]);
+        }
+    }
+}
+    
+
+
