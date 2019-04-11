db21f214d25590dacb5d152dd6b762e83d80328e
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-11 22:07:42 +0530

    a program on implementing Let and Const

---
+++ Let_and_Const.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    var r;
+    r = readLine();
+    var area = PI * r * r;
+    var perimeter = PI * r * 2;
+    console.log(area);
+    console.log(perimeter);
+
+
+    
+    // Print the area of the circle:
+    
+    // Print the perimeter of the circle:
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
