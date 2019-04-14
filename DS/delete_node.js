efb9504538b7b5ad426f89ca4743734c2cd7f6c3
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-14 23:33:50 +0530

    code for deleting a particular node

---
+++ DS/delete_node.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function deleteNode(head, position) {
+    var ptr = new SinglyLinkedList;
+    var ptrnext = new SinglyLinkedList;
+    ptr = head;
+    var i = 0;
+    if (position == 0) {
+        head = head.next;
+        return head;
+    }
+    while (i < position) {
+        ptrnext = ptr;
+        ptr = ptr.next;
+        i++;
+    }
+    ptrnext.next = ptr.next;
+    return head;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const llistCount = parseInt(readLine(), 10);
+
+    let llist = new SinglyLinkedList();
+
+    for (let i = 0; i < llistCount; i++) {
+        const llistItem = parseInt(readLine(), 10);
+        llist.insertNode(llistItem);
+    }
+
+    const position = parseInt(readLine(), 10);
+
+    let llist1 = deleteNode(llist.head, position);
+
+    printSinglyLinkedList(llist1, " ", ws)
+    ws.write("\n");
+
+    ws.end();
+}
