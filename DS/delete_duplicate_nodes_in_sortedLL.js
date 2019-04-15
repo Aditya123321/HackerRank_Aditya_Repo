8de66d53461d8c0325ff99e076c4175b55a06693
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-15 16:17:29 +0530

    code for finding duplicates

---
+++ DS/delete_duplicate_nodes_in_sortedLL.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function removeDuplicates(head) {
+
+    var current = head;
+
+    var node = head;
+
+
+
+
+
+    if (head == null)
+
+        return;
+
+
+
+    /* Traverse list till the last node */
+
+    while (current.next != null) {
+
+
+
+        /*Compare current node with the next node */
+
+        if (current.data == current.next.data) {
+
+            node = current.next.next;
+
+            current.next = null;
+
+            current.next = node;
+
+        }
+
+        else {
+
+            current = current.next;
+
+        }
+
+    }
+
+
+
+    return head;
+}
+
+function main() {
