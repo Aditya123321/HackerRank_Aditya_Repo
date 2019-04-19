749c3eae7ef7eb51a8281b373aded6e6ea712000
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-19 11:09:17 +0530

    code for calculating the no of candles that can be blown

---
+++ Algorithms/birthday_cake_candles.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthdayCakeCandles function below.
+function birthdayCakeCandles(ar) {
+    var max = ar[0];
+    var count = 0;
+    for (var x = 0; x < ar.length; x++){
+        if (ar[x] > max) {
+            max = ar[x];
+        }
+    }
+    for (var x = 0; x < ar.length; x++) {
+        if (ar[x] == max) {
+            count++;
+        }
+    }
+    return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = birthdayCakeCandles(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
