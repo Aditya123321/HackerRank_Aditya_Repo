22b8f0a370e3958386e14f0c371270876d7391b4
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-19 11:13:39 +0530

    code for calculating the array sum

---
+++ Algorithms/simple_array_sum.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    var ar;
+    var sum = 0;
+    for (var x = 0; x < ar.length; x++) {
+        sum = sum + ar[x];
+    }
+//    console.log(sum);
+    return sum;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = simpleArraySum(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
