aee2592cbd43b420e9310498e72b33f1d8d4fce8
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-19 11:12:32 +0530

    code for calculating big factorial

---
+++ Algorithms/extra_long_factorial.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+
+    BigInteger fact;
+    fact = new BigInteger("1");
+    for (int x = 1; x <= n; x++){
+        fact = fact.multiply(BigInteger.valueOf(x));
+    }
+    System.out.print(fact);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
